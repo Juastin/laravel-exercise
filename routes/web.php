39228ee9c60add12ef16fd78b0de32a34aeba724
Justin Passchier <juspasschier@gmail.com>
2021-11-13 10:16:45 +0100

    Added route to header and created new route for Products

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/products', function () {
+    return view('products');
+})->middleware(['auth'])->name('products');
+
 require __DIR__.'/auth.php';
